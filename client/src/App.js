4835006319690147650ef2582fb02d256ae899e6
aryan <aryansatija2003@gmail.com>
2024-02-09 22:22:19 +0530

    added initial jsx files

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,16 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Login, Signup, Validate, Create, Dashboard } from './pages';
 function App() {
   return (
     <div >
-    
+      <Routes>
+        <Route exact path="/" element={<Dashboard/>}/>
+        <Route exact path="/login" element={<Login/>}/>
+        <Route exact path="/signup" element={<Signup/>}/>
+        <Route exact path="/validate" element={<Validate/>}/>
+        <Route exact path="/create" element={<Create/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ client/src/pages/index.js
@@ -0,0 +1,6 @@
+import { Login } from './login.js';
+import { Signup } from './signup.js';
+import { Dashboard } from './dashboard.js';
+import { Validate } from './validate.js';
+import { Create } from './create.js';
+export {Login, Signup, Dashboard, Validate, Create};

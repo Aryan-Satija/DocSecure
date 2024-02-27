1016088a6615b237f6ceeaa9183f63f6ca3234f5
aryan <aryansatija2003@gmail.com>
2024-02-27 16:01:26 +0530

    redux setup

--- client/src/index.js
+++ client/src/index.js
@@ -3,15 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { store } from './app/store.js';
+import { Provider } from 'react-redux';
 import {BrowserRouter} from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
+import {ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-    <ToastContainer/>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+      <ToastContainer/>
+    </BrowserRouter>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function

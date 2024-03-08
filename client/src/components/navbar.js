4d6e2457004b38a927a2b4cb971f6894e457f666
aryan <aryansatija2003@gmail.com>
2024-03-08 20:08:07 +0530

    integration of client side ethers package

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,26 +1,48 @@
 import React from 'react'
 import { MdVerifiedUser, MdDarkMode } from "react-icons/md";
 import {useNavigate, Link} from 'react-router-dom';
+import { UseSelector, useSelector } from 'react-redux';
 export const Navbar = () => {
     const navigate = useNavigate();
+    const token = useSelector(state => state.auth.token)
     return (
         <div className='flex items-center justify-between p-4 w-full border fixed bg-white z-20'>
             <div>   
                 <MdVerifiedUser className='size-8 text-green-500'/>
             </div>
             <div className='flex gap-4 w-[60%]'>
-                <Link to="" className='flex-1 text-center cursor-pointer border rounded-md p-2 hover:bg-gray-500/20 hover:backdrop-blur-md duration-500'>Profile</Link>
-                <Link to="/create" className='flex-1 text-center cursor-pointer border rounded-md p-2 hover:bg-gray-500/20 hover:backdrop-blur-md duration-500'>Create</Link>
-                <Link to="/validate" className='flex-1 text-center cursor-pointer border rounded-md p-2 hover:bg-gray-500/20 hover:backdrop-blur-md duration-500'>Validate</Link>
-                <Link to="" className='flex-1 text-center cursor-pointer border rounded-md p-2 hover:bg-gray-500/20 hover:backdrop-blur-md duration-500'>Key</Link>
-                <Link to="/home" className='flex-1 text-center cursor-pointer border rounded-md p-2 hover:bg-gray-500/20 hover:backdrop-blur-md duration-500'>Home</Link>
-                <Link to="" className='flex-1 text-center cursor-pointer border rounded-md p-2 hover:bg-gray-500/20 hover:backdrop-blur-md duration-500'>About</Link>
-                <Link to="" className='flex-1 text-center cursor-pointer border rounded-md p-2 hover:bg-gray-500/20 hover:backdrop-blur-md duration-500'>Contact Us</Link>
+                {
+                    token && <Link to="" className='flex-1 text-center cursor-pointer border rounded-md p-2 hover:bg-gray-500/20 hover:backdrop-blur-md duration-500'>Profile</Link>
+                }
+                {
+                    token && <Link to="/create" className='flex-1 text-center cursor-pointer border rounded-md p-2 hover:bg-gray-500/20 hover:backdrop-blur-md duration-500'>Create</Link>
+                }
+                {
+                    token && <Link to="/validate" className='flex-1 text-center cursor-pointer border rounded-md p-2 hover:bg-gray-500/20 hover:backdrop-blur-md duration-500'>Validate</Link>
+                }
+                {
+                    token && <Link to="" className='flex-1 text-center cursor-pointer border rounded-md p-2 hover:bg-gray-500/20 hover:backdrop-blur-md duration-500'>Key</Link>
+                }
+                {
+                    !token && <Link to="/home" className='flex-1 text-center cursor-pointer border rounded-md p-2 hover:bg-gray-500/20 hover:backdrop-blur-md duration-500'>Home</Link>
+                }
+                {
+                    !token && <Link to="" className='flex-1 text-center cursor-pointer border rounded-md p-2 hover:bg-gray-500/20 hover:backdrop-blur-md duration-500'>About</Link>
+                }
+                {
+                    !token && <Link to="" className='flex-1 text-center cursor-pointer border rounded-md p-2 hover:bg-gray-500/20 hover:backdrop-blur-md duration-500'>Contact Us</Link>
+                }
             </div>
             <div className='flex items-center gap-4 justify-center'>
-                <div className='cursor-pointer border rounded-md p-2 hover:bg-gray-500/20 hover:backdrop-blur-md duration-500' onClick={()=>{navigate('/login')}}>Login</div>
-                <div className='cursor-pointer border rounded-md p-2 hover:bg-gray-500/20 hover:backdrop-blur-md duration-500' onClick={()=>{navigate('/signup')}}>Sign Up</div>
-                <div className='cursor-pointer'><MdDarkMode className='size-6 text-black-500'/></div>
+                {
+                    !token && <div className='cursor-pointer border rounded-md p-2 hover:bg-gray-500/20 hover:backdrop-blur-md duration-500' onClick={()=>{navigate('/login')}}>Login</div>
+                }
+                {
+                    !token && <div className='cursor-pointer border rounded-md p-2 hover:bg-gray-500/20 hover:backdrop-blur-md duration-500' onClick={()=>{navigate('/signup')}}>Sign Up</div>
+                }
+                {
+                    token && <div className='cursor-pointer border rounded-md p-2 hover:bg-gray-500/20 hover:backdrop-blur-md duration-500'>Connect To Wallet</div>
+                }
             </div>
         </div>
     )
--- client/src/index.js
+++ client/src/index.js
@@ -8,13 +8,16 @@ import { Provider } from 'react-redux';
 import {BrowserRouter} from 'react-router-dom';
 import {ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { MyProvider } from './context/index.js';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-      <ToastContainer/>
-    </BrowserRouter>
+    <MyProvider>
+      <BrowserRouter>
+        <App />
+        <ToastContainer/>
+      </BrowserRouter>
+    </MyProvider>
   </Provider>
 );
 

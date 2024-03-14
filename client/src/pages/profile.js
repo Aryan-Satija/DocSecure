cbaa2366d4710fef82c4a2222677741d3fe075f5
aryan <aryansatija2003@gmail.com>
2024-03-14 11:30:50 +0530

    profile page structure

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -18,8 +18,41 @@ export const Profile = () => {
   console.log(profile);
 
   return (
-    <div>
-        <div></div>
+    <div className='pl-4 pt-20 flex flex-col gap-4'>
+        <div>
+            <img src={profile?.image} className='w-40 rounded-full'/>
+        </div>
+        <div className='flex items-center gap-4 text-xl'>
+          <div className='text-slate-950/70 w-40 p-2'>Username:</div>
+          <div className='text-slate-700'>
+              {profile?.username}
+          </div>
+        </div>
+        <div className='flex items-center gap-4 text-xl'>
+          <div className='text-slate-950/70 w-40 p-2'>Email:</div>
+          <div className='text-slate-700'>
+              {profile?.email}
+          </div>
+        </div>
+        <div className='flex items-center gap-4 text-xl'>
+          <div className='text-slate-950/70 w-40 p-2'>Account Type:</div>
+          <div className='text-slate-700'>
+              {profile?.accountType}
+          </div>
+        </div>
+        <div className='flex items-center gap-4 text-xl'>
+          <div className='text-slate-950/70 w-40 p-2'>Public Key:</div>
+          <div className='text-slate-700'>
+              {profile?.public_key.substr(0, 110)}
+              ....
+          </div>
+        </div>
+        <div className='flex items-center gap-4 text-xl'>
+          <div className='text-slate-950/70 w-40 p-2'>Private Key:</div>
+          <div className='text-slate-700 border rounded-md p-2 bg-sky-400 cursor-pointer duration-200 hover:scale-95'>
+              Generate Private Key            
+          </div>
+        </div>
     </div>
   )
 }

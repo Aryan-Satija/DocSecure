f6e49a42df4a4ce1d02c4ec171109ad7b85f9650
aryan <aryansatija2003@gmail.com>
2024-03-24 15:30:31 +0530

    deployment

--- client/src/services/pdf_apis.js
+++ client/src/services/pdf_apis.js
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.SERVER_BASE_URL || `http://localhost:8080/api/v1`;
+const BASE_URL = process.env.SERVER_BASE_URL || `https://docsecure.onrender.com/api/v1`;
 
 export const PDF_APIS = {
     encrypt_pdf_api : BASE_URL + `/pdf/encrypt-my-pdf`,
